b70580a2c10da3952117ddf0f2517e6ebcc158f9
Aarya Kasbe <aaryakasbe05@gmail.com>
2025-03-14 18:55:52 +0530

    fixed error

--- app/ui/font.ts
+++ app/ui/font.ts
@@ -1,6 +1,9 @@
 import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 
-export const lui=Lusitana({weight:['400']});
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
  
 export const inter = Inter({ subsets: ['latin'] });
